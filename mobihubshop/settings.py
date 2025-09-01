561dbd3b5e158480a17090909b0d7a85908ef223
sabirzafar <sabir_zafar@yahoo.com>
2025-09-01 08:46:31 +0400

    MY SQL configration and Migrated

--- mobihubshop/settings.py
+++ mobihubshop/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'mobihubshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'USER': 'root',
+        'PASSWORD': 'pakistan1947',
     }
 }
 
--- store/models.py
+++ store/models.py
@@ -26,6 +26,7 @@ class Collection(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    slug = models.SlugField(null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -110,5 +111,6 @@ class Cartitem(models.Model):
 class Address (models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=100)
+    zip = models.IntegerField(null=True)
     customer = models.ForeignKey(
         "Customer", on_delete=models.CASCADE)
